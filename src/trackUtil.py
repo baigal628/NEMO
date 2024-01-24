1a09f79389e406fb3a6016da16a2a020b51c0b15
Gali Bai <gali.bai@hotmail.com>
2024-01-23 16:48:56 -0800

    update bedgraph function

--- src/trackUtil.py
+++ src/trackUtil.py
@@ -1,81 +1,60 @@
 import numpy as np
 
-def writeBedGraph(bedGraphHeader, count, coverage, bins, step, outfile, chrom, qstart, qend, track_name = '', track_color = '', normalize = True):
+def predToBedGraph(infile, chr, bins, step, thred=0.5, outpath='', prefix =''):
     
-    thisheader = bedGraphHeader
-    
-    if track_name:
-        thisheader['name'] = track_name
-    if track_color:
-        thisheader['color'] = track_color
-    
-    outFh = open(outfile, 'w')
-    
-    for k,v in bedGraphHeader.items():
-        if v:
-            line = k + '=' + v + ' '
-            outFh.write(line)
-    outFh.write('\n')
-
-    for i in range(len(bins)):
-        start, end = bins[i], bins[i]+step
-        if coverage[i] == 0:
-            binHeight = 0
-        elif normalize:
-            binHeight = "%.3f" % (count[i]/coverage[i])
-        else:
-            binHeight = count[i]
-        line = '{chr}\t{start}\t{end}\t{binheight}\n'.format(chr = chrom, start = start,  end = end, binheight = binHeight)
-        outFh.write(line)
-    outFh.close()
-
-def predictionToBedGraph(prediction, bins, step, threshold, chrom, qstart, qend, prefix, outfile):
-
-    count, cov = np.zeros(len(bins), dtype = int), np.zeros(len(bins), dtype = int)
-    
-    bedGraphHeader = {'track type':'bedGraph', 
-                  'name':'Nemo prediction', 
-                  'description':'addseq',
-                  'visibility':'', 
-                  'color':'r', 
-                  'altColor':'r', 
-                  'priority':'', 
-                  'autoScale':'off', 
-                  'alwaysZero':'off', 
-                  'gridDefault':'off', 
-                  'maxHeightPixels':'default', 
-                  'graphType':'bar',
-                  'viewLimits':'upper',
-                  'yLineMark':'',
-                  'yLineOnOff':'on',
-                  'windowingFunction':'mean',
-                  'smoothingWindow':'on'
-                 }
-
-    
-    with open(prediction, 'r') as predFh:
+    scoreDict = {i:[] for i in range(len(bins))}
+   
+    with open(infile, 'r') as predFh:
         for line in predFh:
             line = line.strip().split('\t')
             readname = line[0]
             strand = line[1]
-            binStart = int(line[2])
-            if binStart > bins[-1]:
+            start = int(line[2])
+            if start > bins[-1]:
                 continue
             probs = line[3].split(',')
-            binEnd = binStart + step*(len(probs)-1)
-            if binEnd < bins[0]:
+            end = start + step*(len(probs)-1)
+            if end < bins[0]:
                 continue
             else:
-                i = int((binStart-bins[0])/step)
+                i = int((start-bins[0])/step)
                 if i < 0:
                     probs = probs[-i:]
                     i = 0
                 for prob in probs:
-                    if float(prob)>= float(threshold):
-                        count[i]+=1
-                    cov[i] +=1
-                    i+=1
+                    prob = float(prob)
+                    if np.isnan(prob):
+                        continue
+                    score = 1 if prob > thred else 0
+                    scoreDict[i].append(score)
+                    i +=1
                     if i >= len(bins):
                         break
-    writeBedGraph(bedGraphHeader=bedGraphHeader, count=count , coverage=cov, bins=bins, step=step, 
-                  chrom= chrom, qstart=qstart, qend=qend, outfile=outfile, track_name=prefix, track_color = 'blue')
+    outfile = open(outpath+prefix+'.bedgraph', 'w')
+    
+    for i in scoreDict:
+        start = bins[i]
+        end = start+step
+        avescore = sum(scoreDict[i])/len(scoreDict[i])
+        outfile.write(chr + '\t' + str(start) + '\t' + str(end) + '\t' + str(avescore) + '\n')
+    
+    outfile.close()
+
+
+def reformatBedgraph(bdg):
+
+    prefix = bdg.split('.bedgraph')[0]
+    outFh = open(prefix+'_1bp.bedgraph', 'w')
+    
+    with open(bdg, 'r') as inputFh:
+        for line in inputFh:
+            line = line.strip().split('\t')
+            chrom = line[0]
+            start = int(line[1])
+            end = int(line[2])
+            value = float(line[3])
+            # Convert the interval into 1 base pair intervals
+            for pos in range(start, end):
+                outFh.write('{chrom}\t{start}\t{end}\t{value}\n'.format(chrom = chrom, start = pos, end = pos+1, value = value))
+    
+    outFh.close()
